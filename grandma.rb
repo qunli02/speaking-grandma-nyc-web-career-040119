cc76f19701301d2d96b2a56056b5d5cb3c7e863b
Qun Li Huang <qunli02@gmail.com>
2019-03-20 04:45:17 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -18,6 +18,7 @@ def speak_to_grandma(phrase)
     return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.upcase
     print "NO, NOT SINCE 1938!"
+    return "NO, NOT SINCE 1938!"
   else
     print "HUH?! SPEAK UP, SONNY!"
   end
